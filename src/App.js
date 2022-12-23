20cec2e1ed939cfe85b39dcfcf720b0340b3b694
Fe Macaraeg <femacaraeg@gmail.com>
2022-12-23 17:50:14 +0800

    Add mobile responsive styling.

--- src/App.js
+++ src/App.js
@@ -7,19 +7,28 @@ import './App.css';
 
 function App() {
   const [name, setName] = useState('Jane Appleseed');
-  const [cardNumber, setCardNumber] = useState('');
+  const [cardNumber, setCardNumber] = useState('0000');
+  const [expMonth, setExpMonth] = useState('00');
+  const [expYear, setExpYear] = useState('00');
 
   const handleFormChange = (props) => {
     setName(props.name);
     setCardNumber(props.number);
+    setExpMonth(props.month);
+    setExpYear(props.year);
   };
 
   console.log(name);
 
   return (
     <div className='App'>
-      <Header name={name} number={cardNumber} />
-      <div>
+      <Header
+        name={name}
+        number={cardNumber}
+        expMonth={expMonth}
+        expYear={expYear}
+      />
+      <div className='Form'>
         <Form onFormChange={handleFormChange} />
       </div>
     </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,33 @@
 import React from 'react';
 
-import mobileBg from '../../assets/bg-main-mobile.png';
+// import mobileBg from '../../assets/bg-main-mobile.png';
 
 import styles from './Header.module.css';
 
 function Header(props) {
-  const { name, number } = props;
+  const { name, number, expMonth, expYear } = props;
   return (
     <div className={styles.container}>
-      <div className={styles.fill}>
-        <img
-          src={mobileBg}
-          className={styles.mobileBg}
-          alt='mobile background'
-        />
-        <div className={styles.cardBack} />
-        <div className={styles.cardFront}>
-          <p>{name ? name : 'Jane Appleseed'}</p>
+      <div className={styles.cardContainer}>
+        <div className={styles.innerCardContainer}>
+          <div className={styles.cardBack} />
+          <div className={styles.cardFront}>
+            <p>{name ? name : 'Jane Appleseed'}</p>
+            <div className={styles.cardNumber}>
+              {Array.from(number).map((val, index) => {
+                return <p key={index}>{val.length !== 0 ? val : '0'}</p>;
+              })}
+            </div>
+            <div>
+              {Array.from(expMonth).map((val, index) => {
+                console.log(expMonth, val);
+                return <p key={index}>{val ? val : '0'}</p>;
+              })}
+            </div>
+            <div>
+              <p>{expYear ? expYear : '00'}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
