2f06fc444e20fdcde67f105fdc84c3b58a7fc933
Fe Macaraeg <femacaraeg@gmail.com>
2023-01-04 20:52:54 +0800

    Updates

---
+++ precache-manifest.d6719fa2fcd3fa0926304f9fe7171e8c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "85085badfb9601c97f3a",
+    "url": "/static/css/main.684bbbe3.chunk.css"
+  },
+  {
+    "revision": "85085badfb9601c97f3a",
+    "url": "/static/js/main.85085bad.chunk.js"
+  },
+  {
+    "revision": "af61781b6f3e55d4eb27",
+    "url": "/static/js/1.af61781b.chunk.js"
+  },
+  {
+    "revision": "052eaa020d5d2b532895",
+    "url": "/static/js/2.052eaa02.chunk.js"
+  },
+  {
+    "revision": "fad66d1fdb7c0ec5e41a",
+    "url": "/static/js/runtime~main.fad66d1f.js"
+  },
+  {
+    "revision": "33d6bbef0946f7071752c239b14d4a67",
+    "url": "/static/media/bg-card-front.33d6bbef.png"
+  },
+  {
+    "revision": "1a41a92ca7b04ab1362f1b2f6bb2a83a",
+    "url": "/static/media/bg-card-back.1a41a92c.png"
+  },
+  {
+    "revision": "530df842c362d952de57b6d262361efc",
+    "url": "/static/media/icon-complete.530df842.svg"
+  },
+  {
+    "revision": "b808367bed37736abc3bfc956a745e59",
+    "url": "/index.html"
+  }
+];
