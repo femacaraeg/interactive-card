cdb28c6e142d3579650351a59850c8e7ab056242
Fe Macaraeg <femacaraeg@gmail.com>
2023-01-05 12:01:57 +0800

    Updates

---
+++ precache-manifest.e0a9b75c815556c2a2cbb1247649a518.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c638a0e72532ef03feb",
+    "url": "/interactive-card/static/css/main.069fa1c1.chunk.css"
+  },
+  {
+    "revision": "8c638a0e72532ef03feb",
+    "url": "/interactive-card/static/js/main.8c638a0e.chunk.js"
+  },
+  {
+    "revision": "3b39c44554f7beae8685",
+    "url": "/interactive-card/static/js/1.3b39c445.chunk.js"
+  },
+  {
+    "revision": "8e9986b66aef16afb43e",
+    "url": "/interactive-card/static/js/2.8e9986b6.chunk.js"
+  },
+  {
+    "revision": "7aa3f4285f854576a2dd",
+    "url": "/interactive-card/static/js/runtime~main.7aa3f428.js"
+  },
+  {
+    "revision": "33d6bbef0946f7071752c239b14d4a67",
+    "url": "/interactive-card/static/media/bg-card-front.33d6bbef.png"
+  },
+  {
+    "revision": "3b8e5db1428efb79aa0006dfbef1d690",
+    "url": "/interactive-card/static/media/bg-main-mobile.3b8e5db1.png"
+  },
+  {
+    "revision": "1a41a92ca7b04ab1362f1b2f6bb2a83a",
+    "url": "/interactive-card/static/media/bg-card-back.1a41a92c.png"
+  },
+  {
+    "revision": "530df842c362d952de57b6d262361efc",
+    "url": "/interactive-card/static/media/icon-complete.530df842.svg"
+  },
+  {
+    "revision": "88e46d3eb3708402c9a83c9187572444",
+    "url": "/interactive-card/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4513fcff5d82fb6811457bdf21c8b875.js"
+  "/interactive-card/precache-manifest.e0a9b75c815556c2a2cbb1247649a518.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/interactive-card/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
